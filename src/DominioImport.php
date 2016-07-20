e4ef1f256cbc49e458cc6dbe19c7aaeaeb213c88
Eduardo Bizarro <edbizarro@gmail.com>
2016-07-20 16:53:42 -0300

    package boostrap

---
+++ src/DominioImport.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Convenia\DominioImport;
+
+/**
+ * Class DominioImport
+ */
+class DominioImport
+{
+
+}
---
+++ tests/BaseTest.php
@@ -0,0 +1,11 @@
+<?php
+namespace Convenia\DominioImport\Tests;
+
+use PHPUnit_Framework_TestCase;
+
+/**
+ * Class BaseTest.
+ */
+abstract class BaseTest extends PHPUnit_Framework_TestCase
+{
+}
