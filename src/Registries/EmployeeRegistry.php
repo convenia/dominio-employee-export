a1604af2cc18091f6a9a3d3571458cea50065baf
Alex Assunção <alex.assuncao@convenia.com.br>
2018-06-18 13:36:08 -0300

    WIP: Obrigatorios

--- src/Registries/EmployeeRegistry.php
+++ src/Registries/EmployeeRegistry.php
@@ -176,10 +176,290 @@ class EmployeeRegistry extends Registry
             'position' => 191,
             'length'   => 1,
         ],
-        'contrib_indiv' => [
+        'RECEBIA_SEGURO_DESEMPREGO' => [
             'format'   => FieldC::class,
-            'position' => 191,
+            'position' => 192,
+            'length'   => 7,
+        ],
+        'INCLUSAO_VALIDADA_ESOCIAL' => [
+            'format'   => FieldC::class,
+            'position' => 199,
+            'length'   => 7,
+        ],
+        'TIPO_INSCRICAO_EMPRESA_ORIGEM_DIRIGENTE_SINDICAL' => [
+            'format'   => FieldC::class,
+            'position' => 206,
+            'length'   => 7,
+        ],
+        'I_AFASTAMENTOS' => [
+            'format'   => FieldC::class,
+            'position' => 213,
+            'length'   => 7,
+        ],
+        'salario' => [
+            'format'   => FieldC::class,
+            'position' => 220,
+            'length'   => 17,
+        ],
+        'CALCULAR_INSS_PATRONAL' => [
+            'format'   => FieldC::class,
+            'position' => 237,
+            'length'   => 7,
+        ],
+        'vinculo' => [
+            'format'   => FieldC::class,
+            'position' => 244,
+            'length'   => 7,
+        ],
+        'ORGAO_EXPEDICAO_RG' => [
+            'format'   => FieldC::class,
+            'position' => 251,
+            'length'   => 7,
+        ],
+        'INCLUSAO_VALIDADA_ESOCIAL_S_2206' => [
+            'format'   => FieldC::class,
+            'position' => 258,
+            'length'   => 7,
+        ],
+        'BENEFICIADO_PAT_REFEICAO_CONVENIO' => [
+            'format'   => FieldC::class,
+            'position' => 265,
+            'length'   => 7,
+        ],
+        'grau_instrucao' => [
+            'format'   => FieldC::class,
+            'position' => 272,
+            'length'   => 7,
+        ],
+        'RESIDENCIA_PROPRIA' => [
+            'format'   => FieldC::class,
+            'position' => 279,
+            'length'   => 7,
+        ],
+        'sexo' => [
+            'format'   => FieldC::class,
+            'position' => 286,
             'length'   => 1,
+        ],
+        'deficiente_fisico' => [
+            'format'   => FieldC::class,
+            'position' => 287,
+            'length'   => 3,
+        ],
+        'ENDERECO_COMERCIAL' => [
+            'format'   => FieldC::class,
+            'position' => 290,
+            'length'   => 7,
+        ],
+        'ENVIAR_ESOCIAL_S_2205' => [
+            'format'   => FieldC::class,
+            'position' => 297,
+            'length'   => 7,
+        ],
+        'TIPO_INDICATIVO_ADMISSAO' => [
+            'format'   => FieldC::class,
+            'position' => 304,
+            'length'   => 7,
+        ],
+        'ENVIAR_ESOCIAL_S_2206' => [
+            'format'   => FieldC::class,
+            'position' => 311,
+            'length'   => 7,
+        ],
+        'oco_sefip' => [
+            'format'   => FieldC::class,
+            'position' => 318,
+            'length'   => 7,
+        ],
+        'SITUACAO_QUALIFICACAO_CADASTRAL_INSS_RECEITA_FEDERAL' => [
+            'format'   => FieldC::class,
+            'position' => 325,
+            'length'   => 7,
+        ],
+        'BENEFICIADO_PAT_REFEICOES_TRANSPORTADAS' => [
+            'format'   => FieldC::class,
+            'position' => 332,
+            'length'   => 7,
+        ],
+        'emprego_ant' => [
+            'format'   => FieldC::class,
+            'position' => 339,
+            'length'   => 7,
+        ],
+        'tempermuta' => [
+            'format'   => FieldC::class,
+            'position' => 346,
+            'length'   => 3,
+        ],
+        'i_ccustos' => [
+            'format'   => FieldC::class,
+            'position' => 349,
+            'length'   => 7,
+        ],
+        'DESLIGAMENTO_ENVIADO_ESOCIAL_REINTEGRACAO' => [
+            'format'   => FieldC::class,
+            'position' => 356,
+            'length'   => 7,
+        ],
+        'DESLIGAMENTO_REALIZADO_MESMO_BANCO_REINTEGRACAO' => [
+            'format'   => FieldC::class,
+            'position' => 363,
+            'length'   => 7,
+        ],
+        'EXCLUIR_EVENTO_S_2206_RETIFICACAO' => [
+            'format'   => FieldC::class,
+            'position' => 370,
+            'length'   => 7,
+        ],
+        'USA_SALARIO_MINIMO_ESTADUAL' => [
+            'format'   => FieldC::class,
+            'position' => 377,
+            'length'   => 7,
+        ],
+        'categoria' => [
+            'format'   => FieldC::class,
+            'position' => 384,
+            'length'   => 7,
+        ],
+        'forma_pagto' => [
+            'format'   => FieldC::class,
+            'position' => 391,
+            'length'   => 1,
+        ],
+        'BENEFICIADO_PAT_ADMINISTRACAO_COZINHAS' => [
+            'format'   => FieldC::class,
+            'position' => 392,
+            'length'   => 7,
+        ],
+        'i_empregados' => [
+            'format'   => FieldC::class,
+            'position' => 399,
+            'length'   => 7,
+        ],
+        'APOSENTADO' => [
+            'format'   => FieldC::class,
+            'position' => 406,
+            'length'   => 7,
+        ],
+        'GERAR_RETIFICACAO_ESOCIAL_S_2206' => [
+            'format'   => FieldC::class,
+            'position' => 413,
+            'length'   => 7,
+        ],
+        'CARGA_HORARIA_VARIAVEL' => [
+            'format'   => FieldC::class,
+            'position' => 420,
+            'length'   => 7,
+        ],
+        'DESCONTAR_ESTOURO_RESCISAO_EMPRESA_ORIGEM' => [
+            'format'   => FieldC::class,
+            'position' => 427,
+            'length'   => 7,
+        ],
+        'GERAR_RETIFICACAO_ESOCIAL' => [
+            'format'   => FieldC::class,
+            'position' => 434,
+            'length'   => 7,
+        ],
+        'contr_sobre_sal_min' => [
+            'format'   => FieldC::class,
+            'position' => 441,
+            'length'   => 3,
+        ],
+        'nome' => [
+            'format'   => FieldC::class,
+            'position' => 444,
+            'length'   => 70,
+        ],
+        'opta_fgts' => [
+            'format'   => FieldC::class,
+            'position' => 514,
+            'length'   => 1,
+        ],
+        'BENEFICIADO_PAT_CESTA_ALIMENTOS' => [
+            'format'   => FieldC::class,
+            'position' => 515,
+            'length'   => 7,
+        ],
+        'RESIDENTE_EXTERIOR' => [
+            'format'   => FieldC::class,
+            'position' => 522,
+            'length'   => 7,
+        ],
+        'ALVARA_JUDICIAL' => [
+            'format'   => FieldC::class,
+            'position' => 529,
+            'length'   => 7,
+        ],
+        'POSSUI_REDUCAO_ALIQUOTA_INSS' => [
+            'format'   => FieldC::class,
+            'position' => 536,
+            'length'   => 7,
+        ],
+        'gps_deducao' => [
+            'format'   => FieldC::class,
+            'position' => 543,
+            'length'   => 3,
+        ],
+        'tipo_epr' => [
+            'format'   => FieldC::class,
+            'position' => 546,
+            'length'   => 3,
+        ],
+        'troca_aut_clas' => [
+            'format'   => FieldC::class,
+            'position' => 549,
+            'length'   => 3,
+        ],
+        'ENVIAR_REGISTRO_APOS_EXCLUSAO_ESOCIAL' => [
+            'format'   => FieldC::class,
+            'position' => 552,
+            'length'   => 7,
+        ],
+        'BENEFICIADO_PAT_SERVICO_PROPRIO' => [
+            'format'   => FieldC::class,
+            'position' => 559,
+            'length'   => 7,
+        ],
+        'possui_vinc_out_emp' => [
+            'format'   => FieldC::class,
+            'position' => 566,
+            'length'   => 1,
+        ],
+        'simples_072007' => [
+            'format'   => FieldC::class,
+            'position' => 567,
+            'length'   => 3,
+        ],
+        'REMUNERACAO_PERIODO_DESLIGAMENTO_REINTEGRACAO_PAGAS_JUIZO' => [
+            'format'   => FieldC::class,
+            'position' => 570,
+            'length'   => 7,
+        ],
+        'horas_dia' => [
+            'format'   => FieldC::class,
+            'position' => 577,
+            'length'   => 5,
+        ],
+        'TRANSFERIDO_COM_EMPREGADO_ORIGEM_VALIDADO_NO_ESOCIAL' => [
+            'format'   => FieldC::class,
+            'position' => 582,
+            'length'   => 7,
+        ],
+        'COTA_DIFICIENTE' => [
+            'format'   => FieldC::class,
+            'position' => 589,
+            'length'   => 7,
+        ],
+        'POSSUI_DEFICIENCIA_INTELECTUAL' => [
+            'format'   => FieldC::class,
+            'position' => 596,
+            'length'   => 7,
+        ],
+        'GPS_CONJUNTA_NOVEMBRO_13_INTEGRAL' => [
+            'format'   => FieldC::class,
+            'position' => 596,
+            'length'   => 7,
         ],
     ];
 }
